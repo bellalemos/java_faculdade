1f4261868b44229b3dc71fff948d42bc989710f2
bellalemos <117674653+bellalemos@users.noreply.github.com>
2023-05-08 10:46:09 -0300

    Create proprietario.java

---
+++ proprietario.java
@@ -0,0 +1,18 @@
+public class Proprietario {
+
+    private String nome;
+    private String telefone;
+
+    public Proprietario(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
